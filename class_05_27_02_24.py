40d4438ded34a36ad95f06c766cbdb842613603c
orang3world <aaorange75@gmail.com>
2024-02-27 16:43:31 -0300

    ejercicio en clase

--- class_05_27_02_24.py
+++ class_05_27_02_24.py
@@ -83,7 +83,7 @@ class SalarioFijo(Empleado):
 
 empl01 = Empleado('12345678',Ariel, Sanchez,1985, PorComision)
 empl02 = Empleado('87654321',Juan, Fernandez,2005, PorComision)
-empl03 = Empleado('12345678',Cecilia, Garcia,2022, PorComision)
-empl04 = Empleado('12345678',Alicia, Mendoza,2009, SalarioFijo)
-empl05 = Empleado('12345678',Fennando , Olavarria,2020, SalarioFijo)
-empl06 = Empleado('12345678',German, Larrea,2000, SalarioFijo)
+empl03 = Empleado('22233344',Cecilia, Garcia,2022, PorComision)
+empl04 = Empleado('33344455',Alicia, Mendoza,2009, SalarioFijo)
+empl05 = Empleado('44455566',Fennando , Olavarria,2020, SalarioFijo)
+empl06 = Empleado('66677788',German, Larrea,2000, SalarioFijo)
